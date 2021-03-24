3be844734be933e8da39fd96e0ba445dee2fb635
Aravind A <aravind.arali@gmail.com>
2021-03-25 01:51:39 +0530

    added some basic CU unverified user APIs

--- backend/src/routers/api/users.js
+++ backend/src/routers/api/users.js
@@ -16,7 +16,7 @@ const router = express.Router();
 
 /* 
     route : "/api/users",
-    desc : "Register as Teacher or Student",
+    desc : "Register as admin(This route doesn't exist)",
     auth : "PUBLIC",
     method: "POST"
 */
@@ -56,33 +56,148 @@ router.post("/",async (req,res)=>{
 });
 
 /* 
-    route : "/api/users",
-    desc : "Update profile",
+    route : "/api/users/add",
+    desc : "Admin adds other users (Teacher,Student)",
+    auth : ["Admin"],
+    method: "POST"
+*/
+
+router.post("/add",auth,async (req,res)=>{
+    try {
+        
+        if(req.user.role!=="admin"){
+            return res.status(401).json({"msg":"Not authorized user!"})
+        }
+
+        const check = await User.find({email:req.body.email});
+        if(check.length!==0){
+            return res.json({"msg":"Email already registered!"})
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password,salt)
+        
+        const user = new User(req.body);
+        await user.save();
+        if(user.role=="admin"){
+            const admin = new Admin({user_id:user.id})
+            await admin.save()
+        }
+        else if(user.role=="teacher"){
+            const teacher = new Teacher({user_id:user.id,usn:req.body.usn});
+            await teacher.save();
+        }else if(user.role=="student"){
+            const student = new Student({user_id:user.id,usn:req.body.usn,semester:req.body.semester});
+            await student.save();
+        }else{
+            return res.status(404).json("invalid user");
+        }
+        
+        res.json({"msg":"User Added"});        
+    } catch (error) {
+        res.status(400).send(error);   
+    }
+});
+
+
+/* 
+    route : "/api/users/admin/me",
+    desc : "Update admin profile",
     auth : ["Admin"],
     method: "PATCH"
 */
 
-router.patch("/",auth,async (req,res)=>{
+router.patch("/admin/me",auth,async (req,res)=>{
     try {
         const user = req.user;
         if(!user || !user.role=="admin"){
-            return res.json({"msg":"User Not Found or no access!"})
+            return res.json({"msg":"unauthorized access!"})
         }
     const updates = req.body;
     delete updates.token;
     delete updates._id;
     delete updates.id;
-    if(updates.password){
-        const salt = await bcrypt.genSalt(10);
-        updates.password = await bcrypt.hash(updates.password,salt)
+    delete updates.password
+    const editUser = await User.findOneAndUpdate({_id:user.id},updates,{new:true}).select("-password")
+    const editAdmin = await Admin.findOneAndUpdate({user_id:user.id},updates,{new:true})
+    
+    if(!editUser && !editAdmin){
+        return res.json({"msg":"admin not found!"})
     }
-    const editUser = await User.findOneAndUpdate({_id:user.id},updates,{new:true}).select("-password")    
-    res.json(editUser);
+    return res.json({user:editUser,admin:editAdmin});
     } catch (error) {
         console.log(error);
         return res.status(500).send();
     }
 });
+
+/* 
+    route : "/api/users/update/:id",
+    desc : "Admin Update user details",
+    auth : ["Admin"],
+    method: "PATCH"
+*/
+
+router.patch("/update/:id",auth,async (req,res)=>{
+    try {
+        const user = req.user;
+        if(!user || !user.role=="admin"){
+            return res.json({"msg":"unauthorized access!"})
+        }
+    
+        const updates = req.body;
+        delete updates.token;
+        delete updates._id;
+        delete updates.id;
+        delete updates.password;
+        const editUser = await User.findOneAndUpdate({_id:req.params.id},updates,{new:true}).select("-password")
+        if(!editUser){
+            return res.json({"msg":"user not found!"})
+        }
+        const editStudentUser = await Student.findOneAndUpdate({user_id:req.params.id},updates,{new:true})
+        const editTeacherUser = await Teacher.findOneAndUpdate({user_id:req.params.id},updates,{new:true})
+        if(editStudentUser){
+            return res.json({user:editUser,student:editStudentUser})
+        }else if(editTeacherUser){
+            return res.json({user:editUser,student:editTeacherUser})
+        }
+        return res.json({"msg":"Update failure"})
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send();
+    }
+});
+
+/* 
+    route : "/api/users/update/me",
+    desc : "user updates his details",
+    auth : ["Teacher","Student"],
+    method: "PATCH"
+*/
+
+router.patch("/update/me",auth,async (req,res)=>{
+    try {
+        const user = req.user;
+        if(!user){
+            return res.status(404).json({"msg":"no user found!"})
+        }
+    
+        const updates = req.body;
+        delete updates.token;
+        delete updates._id;
+        delete updates.id;
+        delete updates.password;
+        const editUser = await User.findOneAndUpdate({_id:req.params.id},updates,{new:true}).select("-password")
+        if(!editUser){
+            return res.json({"msg":"user not found and update failure!"})
+        }
+        return res.json(editUser)
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send();
+    }
+});
+
 
 /* 
     route : "/api/users",
